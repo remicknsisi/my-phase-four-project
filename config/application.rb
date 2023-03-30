08d5997e6ddf06144f42a0fb1be24a4a08eb0af7
Sienna <remicknsisi@gmail.com>
2023-03-30 15:20:49 -0400

    commented out bundler require for deployment

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
 
 module ReactRailsApiProjectTemplate
   class Application < Rails::Application
