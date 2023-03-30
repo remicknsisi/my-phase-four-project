5889554fb8f95b6af7360caecb6b33fc513e93ef
Sienna <remicknsisi@gmail.com>
2023-03-30 15:32:04 -0400

    tweaked for deployment

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,12 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 module ReactRailsApiProjectTemplate
   class Application < Rails::Application
