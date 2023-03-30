509c029b8eb8165d364356a42760dffda71a6526
Sienna <remicknsisi@gmail.com>
2023-03-30 16:30:19 -0400

    connected react view to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :reviews, only: [:index, :show]
   resources :teachers, only: [:index, :show]
   resources :students, only: [:index, :show]
-  root :to => 'students#index'
-
+  # root to: "main#index"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
+
