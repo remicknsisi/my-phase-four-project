3398433acbf4ca36dde4c51a9c852ae886c32081
Sienna <remicknsisi@gmail.com>
2023-03-31 12:37:45 -0400

    added email contact to teacher pages

--- client/src/components/StudentDetails.js
+++ client/src/components/StudentDetails.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function StudentDetails () {
+    const [student, setStudent] = useState({})
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/students/${id}`)
+        .then(res => res.json())
+        .then(studentData => setStudent(studentData))
+    }, []) 
 
     return (
         <div className="student-card-details">
-
+            <h1>{student.name}</h1>
+            <img src={student.image}/>
+            <h2>{student.house}</h2>
         </div>
     )
 }
--- client/src/components/TeacherDetails.js
+++ client/src/components/TeacherDetails.js
@@ -11,9 +11,14 @@ function TeacherDetails () {
         .then(teacherData => setTeacher(teacherData))
     }, []) 
 
+    // const email = teacher.name.replace(/ /g, '').toLowerCase() + '@hogwarts.edu'
+    // console.log(email)
+
     return (
         <div className="teacher-card-details">
             <h1>{teacher.name}</h1>
+            Contact: {!teacher.name ? null : teacher.name.replace(/ /g, '').toLowerCase() + '@hogwarts.edu'}
+            <br/>
             <img src={teacher.image}/>
             <h2>{teacher.house}</h2>
             <h3>Classes being taught this Academic Term:</h3>
